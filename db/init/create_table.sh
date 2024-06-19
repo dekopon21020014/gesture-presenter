9241db1e8dc45cab427037661aff9e71068e5ee8
dekopon21020014 <renueta@icloud.com>
2024-06-19 10:53:53 +0900

    fix: passwordカラムのタイポを修正

--- db/init/create_table.sh
+++ db/init/create_table.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
+$CMD_MYSQL -e "create table users (
+    id int AUTO_INCREMENT NOT NULL primary key,
+    email char(128) NOT NULL,
+    name char(128) NOT NULL,
+    password char(128) NOT NULL
+    );"

66c8ef7ee21480e1bf78529b3c993586d59082a9
Diachi SEGAWA <285102d@gs.kochi-tech.ac.jp>
2024-07-10 18:32:45 +0900

    top-page作成

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,11 +4,12 @@
 import BasicSlider from "./components/BasicSlider/BasicSlider";
 import styles from "./page.module.css";
 import { Mediapipe } from "./components/Detection/Mediapipe";
+import Top from "./top/page";
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <BasicSlider />
+      <Top />
     </div>
   );
 }

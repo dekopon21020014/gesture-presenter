b8e3cb40f9b6d3b65ba66e82718cfcae1c1a2435
teppeiokano <tetsuko1132@icloud.com>
2024-07-18 14:18:57 +0900

    2を押したら右下にgoodエフェクト３秒間表示を追加

--- front/src/app/components/Effects/Effects.tsx
+++ front/src/app/components/Effects/Effects.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import gsap from 'gsap';
 import {playSound} from '../Sounds/Sounds';
+import {Clap} from './clap';
+import {Good} from './good';
 
 export const Effects: React.FC = () => {
   useEffect(() => {
@@ -9,8 +10,10 @@ export const Effects: React.FC = () => {
         playSound('/SoundsRoom/whoo.mp3');
         for (let i = 0; i < 10; i++) {
           const delay = Math.random() * 2000; // 0-1 sec
-          setTimeout(createImage, delay);
+          setTimeout(Clap, delay);
         }
+      } else if (event.key === '2') {
+        Good();
       }
     };
 
@@ -21,26 +24,6 @@ export const Effects: React.FC = () => {
     };
   }, []);
 
-  const createImage = async () => {
-    const img = document.createElement('img');
-    img.src = '/EffectsRoom/clap.png';
-    img.style.position = 'fixed';
-    img.style.whiteSpace = 'nowrap';
-    img.style.left = `${document.documentElement.clientWidth}px`;
-    img.style.zIndex = '9999'; // 最前面に表示するためのz-index
-    const random = Math.round(Math.random() * document.documentElement.clientHeight);
-    img.style.top = `${random}px`;
-    img.style.height = '150px';
-    document.body.appendChild(img);
-
-    await gsap.to(img, {
-      duration: 5, // 端から端まで5秒
-      x: -1 * (document.documentElement.clientWidth + img.clientWidth),
-      ease: 'linear' // 一定のスピードに設定
-    });
-
-    img.parentNode?.removeChild(img);
-  };
   return null; // JSXを返さないためにnullを返す
 };
 

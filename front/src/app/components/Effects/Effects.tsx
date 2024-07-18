45236a51cbdd24ad29c662e268ac4e039738e3a5
teppeiokano <tetsuko1132@icloud.com>
2024-07-18 15:18:48 +0900

    音とエフェクトが同じファイルで指定していたのを別々に分けた

--- front/src/app/components/Effects/Effects.tsx
+++ front/src/app/components/Effects/Effects.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import {playSound} from '../Sounds/Sounds';
 import {Clap} from './clap';
 import {Good} from './good';
 
@@ -7,15 +6,12 @@ export const Effects: React.FC = () => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === '1') {
-        playSound('/SoundsRoom/whoo.mp3');
         for (let i = 0; i < 10; i++) {
           const delay = Math.random() * 2000; // 0-1 sec
           setTimeout(Clap, delay);
         }
       } else if (event.key === '2') {
         Good();
-      } else if (event.key === '3') {
-        playSound('/SoundsRoom/dodon.mp3');
       }
     };
 

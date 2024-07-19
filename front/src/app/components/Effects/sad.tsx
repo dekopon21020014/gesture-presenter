e63ebb150b2a3467223122ea1bcf3cb52c5e4efd
teppeiokano <tetsuko1132@icloud.com>
2024-07-19 20:43:17 +0900

    エフェクト、音声追加

---
+++ front/src/app/components/Effects/sad.tsx
@@ -0,0 +1,14 @@
+export const Sad = async () => {
+    const img = document.createElement('img');
+    img.src = '/EffectsRoom/sad.jpg';
+    img.style.position = 'fixed';
+    img.style.right = '0%';
+    img.style.bottom = '0%';
+    img.style.zIndex = '9999'; // 最前面に表示するためのz-index
+    img.style.height = '300px';
+    document.body.appendChild(img);
+
+    setTimeout(() => {
+        img.parentNode?.removeChild(img);
+    }, 3000);
+  };

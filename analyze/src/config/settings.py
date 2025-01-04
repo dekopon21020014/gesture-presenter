20a36fbd5aed962c6d2438d05cf307e0730cee47
dekopon21020014 <110777220+dekopon21020014@users.noreply.github.com>
2025-01-04 10:43:06 +0900

    fix: CORSのドメインをlocalhostからfrontに変更

--- analyze/src/config/settings.py
+++ analyze/src/config/settings.py
@@ -9,6 +9,6 @@ class Settings:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     CUDA_VISIBLE_DEVICES = "-1"
     TEMP_DIR = "temp"
-    ALLOWED_ORIGINS = ["http://localhost:3000"]
+    ALLOWED_ORIGINS = ["http://front:3000"]
 
 settings = Settings()
